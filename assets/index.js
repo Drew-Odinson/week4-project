95051cb23f9f123a6dfdfea57641c8974a1d79c4
Drew <drew.ledford2022@gmail.com>
2023-08-25 23:46:03 -0500

    Getting closer but still far off

--- assets/index.js
+++ assets/index.js
@@ -1,28 +1,3 @@
-var counter = document.querySelector("#counter");
-var addButton = document.querySelector("#add");
-var subtractButton = document.querySelector("#subtract");
-
-var count = localStorage.getItem("count");
-
-counter.textContent = count;
-
-addButton.addEventListener("click", function () {
-  if (count < 24) {
-    count++;
-    counter.textContent = count;
-    localStorage.setItem("count", count);
-  }
-});
-
-
-// subtractButton.addEventListener("click", function () {
-//   if (count > 0) {
-//     count--;
-//     counter.textContent = count;
-//     localStorage.setItem("count", count);
-//   }
-// });
-
 
 const questions = [
   {
@@ -79,4 +54,6 @@ const questions = [
 
 var currentQuestionIndex = 0;
 var score = 0;
-var timer;
+var timer= 0;
+
+
